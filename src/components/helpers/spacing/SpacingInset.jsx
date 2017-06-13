0f8ecb892356c95a6b21f424fdbd6eec1249a73b
Plsr <christianpoplawski@gmail.com>
2017-06-13 12:15:51 +0200

    Documentation for SpacingInset

--- src/components/helpers/spacing/SpacingInset.jsx
+++ src/components/helpers/spacing/SpacingInset.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
-
+// Local imports
 import { spacing } from '../../../styles/base/spacing'
 
+/**
+ * Returns the desired size of the SpacingInset component.
+ * Throws an error, if the size is not defined. There is no fallback size here
+ * because styling shouldn't happen 'by chance'.
+ *
+ * @param {String} The size form the component's props
+ * @return {spacing} The accroding spacing
+ */
 let getSize = (size) => {
-  return size ? spacing[size] : spacing['m']
+  if(size === undefined)
+    throw new Error('SpacingInset does need an explicit size')
+
+  let pixelSize = spacing[size]
+
+  if(pixelSize === undefined)
+    throw new Error('The provided size for SpacingInset is not valid.')
+
+  return pixelSize
 }
 
-const SpacingInset = (props) => (
-  <div style={{ padding: getSize(props.size)}}>
-    {props.children}
-  </div>
-)
+/**
+ * A wrapper element for inset spacing.
+ * Takes one of the sizes defined in styles/base/spacing.js as a props
+ */
+function SpacingInset(props) {
+  return (
+    <div style={{ padding: getSize(props.size)}}>
+      {props.children}
+    </div>
+  )
+}
 
 export default SpacingInset
