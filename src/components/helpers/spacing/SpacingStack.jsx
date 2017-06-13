16528a8d039f9871a3c501cac9fd8b0d753af37c
Plsr <christianpoplawski@gmail.com>
2017-06-13 19:36:39 +0200

    DRYed out spacing components

--- src/components/helpers/spacing/SpacingStack.jsx
+++ src/components/helpers/spacing/SpacingStack.jsx
@@ -1,28 +1,6 @@
 import React from 'react'
 // Local imports
-import { spacing } from '../../../styles/base/spacing'
-
-/**
- * Returns the desired size of the SpacingInset component.
- * Throws an error, if the size is not defined. There is no fallback size here
- * because styling shouldn't happen 'by chance'.
- *
- * @param {String} The size form the component's props
- * @return {spacing} The accroding spacing
- *
- * FIXME: DRY
- */
-let getSize = (size) => {
-  if(size === undefined)
-    throw new Error('SpacingInset does need an explicit size')
-
-  let pixelSize = spacing[size]
-
-  if(pixelSize === undefined)
-    throw new Error('The provided size for SpacingInset is not valid.')
-
-  return pixelSize
-}
+import { getSize } from '../../../utils/spacingHelper.js'
 
 /**
  * A wrapper element for inset spacing.
---
+++ src/utils/spacingHelper.js
@@ -0,0 +1,22 @@
+import { spacing } from '../styles/base/spacing'
+
+/**
+ * Checks if the given size is valid and returns its pixel value.
+ * Throws an error, if the size is not defined or invalid. There is no fallback size
+ * here because styling shouldn't happen 'by chance'.
+ *
+ * @param {String} The size form the component's props
+ * @return {spacing} The accroding spacing
+ *
+ */
+export function getSize(size) {
+  if(size === undefined)
+    throw new Error('Spacing components need an explicit size')
+
+  let pixelSize = spacing[size]
+
+  if(pixelSize === undefined)
+    throw new Error('The provided size for the spacing component is not valid.')
+
+  return pixelSize
+}
