eb2b7a715a0c0adb745328e85419cf9a950d01c5
Plsr <christianpoplawski@gmail.com>
2017-06-17 10:01:01 +0200

    Moved scopes to own file as constants

--- src/components/helpers/constants/setupOptions.js
+++ src/components/helpers/constants/setupOptions.js
@@ -1,34 +1,35 @@
 import {ICONS} from './icons.js'
+import {SCOPES} from './scopes.js'
 
 export const setupOptions = [
   {
     icon: ICONS.SMARTPHONE,
     text: 'Native App',
-    value: 'NATIVE_APP',
+    value: SCOPES.NATIVE_APP,
     shouldDisplayForScope: undefined
   },
   {
     icon: ICONS.WEBSITE,
     text: 'Website',
-    value: 'WEBSITE',
+    value: SCOPES.WEBSITE,
     shouldDisplayForScope: undefined
   },
   {
     icon: ICONS.PAGE,
     text: 'Text Document',
-    value: 'TEXT_DOCUMENT',
+    value: SCOPES.TEXT_DOCUMENT,
     shouldDisplayForScope: undefined
   },
   {
     icon: ICONS.ANDROID,
     text: 'Android',
-    value: 'ANDROID',
+    value: SCOPES.ANDROID,
     shouldDisplayForScope: 'NATIVE_APP'
   },
   {
     icon: ICONS.IOS,
     text: 'iOS',
-    value: 'IOS',
+    value: SCOPES.IOS,
     shouldDisplayForScope: 'NATIVE_APP'
   },
   {
