d50e6d7709280c02b296aa060be52d107760dafd
Plsr <christianpoplawski@gmail.com>
2017-06-18 12:56:16 +0200

    Added outline styles for secondary button

--- src/components/SecondaryButton.jsx
+++ src/components/SecondaryButton.jsx
@@ -6,12 +6,21 @@ import { fontSizes, fontWeights } from '../styles/base/fonts.js'
 import { baseColors, appColors } from '../styles/base/colors.js'
 import SpacingSquishedInset from './helpers/spacing/SpacingSquishedInset.jsx'
 
+function setVariantStyles(variant) {
+  if (variant == 'outline') {
+    return styles.buttonOutlineStyles
+  }
+
+  return styles.buttonSolidStyles
+}
+
 function SecondaryButton(props) {
   return(
     <button
       className={css(
         styles.buttonReset,
-        styles.buttonStyles,
+        styles.buttonBaseStyles,
+        setVariantStyles(props.variant),
         props.inactive && styles.buttonInactiveStyles
       )}
       onClick={props.inactive ? '' : props.onClick}
@@ -27,19 +36,32 @@ const styles = StyleSheet.create({
   buttonReset: {
     ...buttonReset
   },
-  buttonStyles: {
+  buttonBaseStyles: {
     fontSize: fontSizes.m,
     fontWeight: fontWeights.semiBold,
-    color: baseColors.white,
-    backgroundColor: appColors.secondary,
-    borderColor: appColors.secondaryDarkened,
     borderWidth: '1px',
     borderStyle: 'solid',
     borderRadius: '4px',
     lineHeight: 1,
-    cursor: 'pointer',
+    cursor: 'pointer'
+  },
+  buttonSolidStyles: {
+    color: baseColors.white,
+    backgroundColor: appColors.secondary,
+    borderColor: appColors.secondaryDarkened,
     ':hover': {
       backgroundColor: appColors.secondaryDarkened
+    },
+    ':focus': {
+      outline: 'none'
+    }
+  },
+  buttonOutlineStyles: {
+    color: baseColors.secondary,
+    borderColor: appColors.secondary,
+    ':hover': {
+      backgroundColor: appColors.secondary,
+      color: baseColors.white
     },
     ':focus': {
       outline: 'none'
