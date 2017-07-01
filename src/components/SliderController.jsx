bba026efee4cae987925acc7496c930c3dd5ba05
Plsr <christianpoplawski@gmail.com>
2017-07-01 11:52:30 +0200

    Basic markup for the SliderController

--- src/components/SliderController.jsx
+++ src/components/SliderController.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {StyleSheet, css} from 'aphrodite'
 
 import SliderInput from './SliderInput.jsx'
 import UnitValueDisplay from './UnitValueDisplay.jsx'
@@ -7,12 +8,18 @@ import UnitValueDisplay from './UnitValueDisplay.jsx'
 function SliderController(props) {
   return(
     <div>
-      <span>{props.title}</span>
+      <span className={css(styles.titleStyles)}>{props.title}</span>
       <span>{props.error}</span>
       <SliderInput value={props.value} handleChange={props.handleChange} storeKey={props.storeKey} />
       <UnitValueDisplay value={props.value} unit={props.unit} />
     </div>
   )
 }
+
+const styles = StyleSheet.create({
+  titleStyles: {
+    display: 'block'
+  }
+})
 
 export default SliderController
