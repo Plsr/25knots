e6fe63fa512cad0f15044c68d4b62c4a8f4df101
Plsr <christianpoplawski@gmail.com>
2017-07-03 12:06:40 +0200

    Possibility to change select

--- src/components/DropdownInput.jsx
+++ src/components/DropdownInput.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function DropdownInput(props) {
+  function handleChange(event) {
+    props.onChange(props.storeKey, event.target.value)
+  }
   return (
-    <select>
+    <select
+      value={props.value}
+      onChange={handleChange}
+    >
       {setOptionTags(props.options)}
     </select>
   )
