e32eaed0b273267f3c55ec10da8db2bef8f2621d
Plsr <christianpoplawski@gmail.com>
2017-07-05 22:33:09 +0200

    Normalized comments

--- src/components/ColorControls.jsx
+++ src/components/ColorControls.jsx
@@ -17,9 +17,11 @@ class ColorControls extends React.Component {
     this.props.onChange(this.props.area, 'contrastPassing', this.setContrastPassing())
   }
 
-  // Check contrast of the two currently picked colors
-  // based on WCAG 2.0 G18
-  // https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20160317/G18
+  /**
+   * Check contrast of the two currently picked colors
+   * based on WCAG 2.0 G18
+   * https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20160317/G18
+   */
   calculateRatio() {
     // Get currently set colors
     let bgColor = tinycolor(this.props.background).toRgb()
@@ -31,9 +33,11 @@ class ColorControls extends React.Component {
     for (var i = 0; i <colors.length; i++) {
       var currColor = colors[i]
 
-      // Convert RGB values of colors to sRGB
-      // Then do claculations as specified in WCAG 2.0 G18
-      // https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20160317/G18#G18-tests
+      /**
+       * Convert RGB values of colors to sRGB
+       * Then do claculations as specified in WCAG 2.0 G18
+       * https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20160317/G18#G18-tests
+       */
       for (var singleColor in currColor) {
         if (currColor.hasOwnProperty(singleColor)) {
           currColor[singleColor] = currColor[singleColor] / 255
@@ -64,6 +68,10 @@ class ColorControls extends React.Component {
     return ratio
   }
 
+  /**
+   * Returns an object with the passing status of the three values
+   * that can be passed to the store.
+   */
   setContrastPassing() {
     let ratio = this.calculateRatio()
     let contrastPassing = {
