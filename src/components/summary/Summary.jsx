dca332f2f9d687038c3dc00b36bf52a13d37f6bf
Plsr <christianpoplawski@gmail.com>
2017-08-05 16:21:18 +0200

    Styled display for scope in summery

--- src/components/summary/Summary.jsx
+++ src/components/summary/Summary.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 import { extractScopeInformation } from '../helpers/functions/scopes.js'
 import {appColors} from '../../styles/base/colors.js'
 import SpacingStack from '../helpers/spacing/SpacingStack.jsx'
+import SpacingInset from '../helpers/spacing/SpacingInset.jsx'
 import SpacingInline from '../helpers/spacing/SpacingInline.jsx'
 import ColorDisplay from '../colors/ColorDisplay.jsx'
 import Icon from '../Icon.jsx'
+import Headline1 from '../shared/Headline1.jsx'
+import Progress from '../shared/Progress.jsx'
+import BorderedBox from '../shared/BorderedBox.jsx'
 import SecondaryButton from '../shared/SecondaryButton.jsx'
 import { generatePDF } from '../helpers/functions/pdfGenerator.js'
 
@@ -18,17 +23,15 @@ class Summary extends React.Component {
     for (var i = 0; i < extractedScopes.length; i++) {
       let currentScope = extractedScopes[i]
       summaryElemets.push(
-        <SpacingStack size='l' key={i} >
-          <div>
-            <SpacingInline size={'l'} >
-              ➜
-            </SpacingInline>
-            <SpacingInline size={'l'} >
-              <Icon icon={currentScope.icon} color={appColors.secondary} size={42} />
-            </SpacingInline>
-            <span>{currentScope.text}</span>
-          </div>
-        </SpacingStack>
+        <div className={css(styles.singleScopeStyles)}>
+          <SpacingInline size={'l'} >
+            ➜
+          </SpacingInline>
+          <SpacingInline size={'l'} >
+            <Icon icon={currentScope.icon} color={appColors.secondary} size={42} />
+          </SpacingInline>
+          <span>{currentScope.text}</span>
+        </div>
       )
     }
 
@@ -63,10 +66,34 @@ class Summary extends React.Component {
 
   render() {
     return (
-      <div id='print'>
-        <p>This is your scope</p>
-
-        {this.buildScopeSummary(this.props.setup.scopes)}
+      <div>
+        <Progress />
+        <SpacingStack size='l' />
+        <SpacingStack size='l'>
+          <div className={css(styles.ContainerStyles)}>
+            <Headline1 content='Your Results' />
+            <p>Following, you can find a summary of what you worked out. <br />
+You can also save this as a pdf for later use.</p>
+            <SpacingStack size='l' />
+            <SecondaryButton
+              onClick={() => {
+                generatePDF(this.props.typography, this.props.colors)
+              }}
+              variant='outline'
+            >
+              Save as PDF
+            </SecondaryButton>
+          </div>
+        </SpacingStack>
+        <BorderedBox>
+          <SpacingInset size='m'>
+            <p>You are building the following:</p>
+            <SpacingStack size='m' />
+            <div className={css(styles.scopesWrapper)}>
+              {this.buildScopeSummary(this.props.setup.scopes)}
+            </div>
+          </SpacingInset>
+        </BorderedBox>
 
         This is everything from Typography <br />
         <ul>
@@ -113,16 +140,28 @@ class Summary extends React.Component {
         <ColorDisplay
           hexVal={'#ffffff'}
         />
-        <SecondaryButton
-          onClick={() => {
-            generatePDF(this.props.typography, this.props.colors)
-          }}
-        >
-          Print
-        </SecondaryButton>
       </div>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  ContainerStyles: {
+    textAlign: 'center'
+  },
+  singleScopeStyles: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '64px',
+    color: appColors.secondary,
+    ':last-child': {
+      marginRight: 0
+    }
+  },
+  scopesWrapper: {
+    display: 'flex',
+    flexDirection: 'row'
+  }
+})
 
 export default Summary
