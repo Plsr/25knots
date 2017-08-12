924cfc4d23c44a5bda0d95197274929482818991
Plsr <christianpoplawski@gmail.com>
2017-08-12 19:32:47 +0200

    Improved colorCalculation.js
    * Improvements in serverel functions
    * Full Documentation

--- src/components/helpers/functions/colorCalculations.js
+++ src/components/helpers/functions/colorCalculations.js
@@ -7,6 +7,12 @@ export const DIRECTIONS = {
   COUNTER_CLOCKWISE: 'COUNTER_CLOCKWISE'
 }
 
+const CHANGABLE_COLOR_ATTRIBUTES = [
+  'LIGHTNESS',
+  'SATURATION',
+  'LIGHTNESS_SATURATION'
+]
+
 // Simple wrapper for the tinycolor toHsl() method
 export function convertToHsl(hexValue) {
   let color = tinycolor(hexValue)
@@ -19,22 +25,31 @@ export function convertToHex(hslObject) {
   return '#' + color.toHex()
 }
 
+// Simple wrapper for the tinycolor toRgb() method
 export function convertToRgb(hexValue) {
   let color = tinycolor(hexValue)
   return color.toRgb()
 }
 
+// Simple wrapper for the tinycolor isLight() function
 export function isLight(hexValue) {
   let color = tinycolor(hexValue)
   return color.isLight()
 }
 
+// Simple wrapper for the tinycolor getBrightness() function
 export function getBrightness(hexValue) {
   let color = tinycolor(hexValue)
   return color.getBrightness()
 }
 
-// TODO: Documentation
+/**
+ * From the set of material color shades, returns the whole object with all shades
+ * for any given shade.
+ *
+ * @param shade The shade of a material color, as hex value
+ * @returns The complete object in which the shade was found
+ */
 export function getMaterialColorObjectForShade(shade) {
   let colorSet = MATERIAL_COLOR_SHADES
 
@@ -53,7 +68,17 @@ export function getMaterialColorObjectForShade(shade) {
   }
 }
 
-// TODO: Documentation
+/**
+ * Calculates a color scheme of monochromatic colors based on a base color with a variable amount of colors.
+ * The colors returned by this function are random in saturation and lightness.
+ * The colors returned by this function are guaranteed to not be equal to either each other, nor the base color.
+ *
+ * NOTE: Since the colors cannot be similar to each other, the amount of options in limited. Therefor, the number of colors to be returned should not be too high (6 will probably still work fine, whereas 25 will cause the function to break.)
+ *
+ * @param amount The number of colors that should be returned
+ * @param baseColor A HEX-Value of a color that is the basis of the color scheme
+ * @returns An Array of HEX-Values that build a monochromatic color scheme
+ */
 export function calculateMonochromaticColors(amount, baseColor) {
 
   let hslColor = convertToHsl(baseColor)
@@ -61,18 +86,24 @@ export function calculateMonochromaticColors(amount, baseColor) {
 
   for (var i = 0; i < amount; i++) {
     let currentColor = Object.assign({}, hslColor)
-    // Figure out if only one or two values should be changed
-    // Random: 0 = Lightness, 1 = Saturation, 2 = Both
-    let randomOption = Math.floor(Math.random() * 3)
+    let randomOption = CHANGABLE_COLOR_ATTRIBUTES[Math.floor(Math.random() * 3)]
     let changedColor = changeValuesOfColor(randomOption, currentColor)
+
+    // Check, if the color is similar to the base color
+    let similarToBaseColor = colorsAreSimilar(hslColor, changedColor)
+    while (similarToBaseColor) {
+      changedColor = changeValuesOfColor(randomOption, changedColor)
+      similarToBaseColor = colorsAreSimilar(hslColor, changedColor)
+    }
 
     if (colors.length < 1) {
       colors.push(convertToHex(changedColor))
     } else {
-      let similarColors = checkForSimilarColors(colors, changedColor)
-      while (similarColors) {
+      // Check, if the new color is similar to other colors that were calculated
+      let similarColorsPresent = colorInArrayIsSimilar(colors, changedColor)
+      while (similarColorsPresent) {
         changedColor = changeValuesOfColor(randomOption, changedColor)
-        similarColors = checkForSimilarColors(colors, changedColor)
+        similarColorsPresent = colorsAreSimilar(colors, changedColor)
       }
       colors.push(convertToHex(changedColor))
     }
@@ -81,24 +112,30 @@ export function calculateMonochromaticColors(amount, baseColor) {
   return colors
 }
 
-// TODO: Documentation
-function changeValuesOfColor(values, color) {
+/**
+ * Changes the lightness and/or saturation value of an HSL color object to a random value and returns a copy of that object.
+ * The random values are restricted to prevent them from beign colorless (i.e. almost black or almost white).
+ * A switch case determines, which attribute(s) of the color should be changed.
+ *
+ * @param attributeToChange The attribute on the color to be changed
+ * @param color An HSL color object on which's hue the new color will be based
+ * @returns An HSL color object with the manipulated attributes
+ */
+function changeValuesOfColor(attributeToChange, color) {
   let manipulatedColor = Object.assign({}, color)
-  // Switch case
-  // Do the changes that need to be DropdownController
-  switch (values) {
-    case 0:
-      // change lightness
-      manipulatedColor.l = Math.random().toFixed(2)
+  let lightnessVal = Math.random() * 0.85 + 0.15
+  let saturationVal = Math.random() * 0.65 + 0.15
+
+  switch (attributeToChange) {
+    case 'LIGHTNESS':
+      manipulatedColor.l = lightnessVal.toFixed(2)
       break
-    case 1:
-      // change Saturation
-      manipulatedColor.s = Math.random().toFixed(2)
+    case 'SATURATION':
+      manipulatedColor.s = saturationVal.toFixed(2)
       break
-    case 2:
-      // change lightning and saturation
-      manipulatedColor.l = Math.random().toFixed(2)
-      manipulatedColor.s = Math.random().toFixed(2)
+    case 'LIGHTNESS_SATURATION':
+      manipulatedColor.l = lightnessVal.toFixed(2)
+      manipulatedColor.s = saturationVal.toFixed(2)
       break
     default:
       throw new 'Oops, seems like the randomizer messed something up.'
@@ -107,19 +144,48 @@ function changeValuesOfColor(values, color) {
   return manipulatedColor
 }
 
-// TODO: Documentation
-function checkForSimilarColors(colors, candidate) {
-  let similarValues = false
-  for (var j = 0; j < colors.length; j++) {
-    let saturationDifference = Math.abs(colors[j].s - candidate.s)
-    let lightnessDifference = Math.abs(colors[j].l - candidate.l)
+/**
+ * Checks if two HSL color objetcs are similar.
+ * Similarity is defined as the Lightness and Saturation being less than 0.1 apart,
+ * with the Hue being exactly the same.
+ *
+ * NOTE: The Hue of the colors is not taken into consideration.
+ *
+ * @param color The first HSL color object
+ * @param candidate The second HSL color object
+ * @returns true if the two colors are found to be similar
+ */
+function colorsAreSimilar(color, candidate) {
+  let saturationDifference = Math.abs(color.s - candidate.s)
+  let lightnessDifference = Math.abs(color.l - candidate.l)
 
-    if (saturationDifference < 0.1 && lightnessDifference < 0.1) {
-      similarValues = true
+  if (saturationDifference < 0.1 && lightnessDifference < 0.1) {
+    return true
+  }
+
+  return false
+}
+
+/**
+ * Checks if an HSL color objetc is similar to any HSL color object in a given array.
+ * To check the similarity itself, the function colorsAreSimilar(color, candidate) is used.
+ *
+ * NOTE: The Hue of the colors is not taken into consideration.
+ *
+ * @param colors An array of HSL color objects
+ * @param candidate The HSL color object to be tested
+ * @returns true if the candidate is similar to any of the objects in the array
+ */
+function colorInArrayIsSimilar(colors, candidate) {
+  let similarColorsPresent = false
+
+  for (var i = 0; i < colors.length; i++) {
+    if (colorsAreSimilar(colors[i], candidate)) {
+      similarColorsPresent = true
     }
   }
 
-  return similarValues
+  return similarColorsPresent
 }
 
 /**
