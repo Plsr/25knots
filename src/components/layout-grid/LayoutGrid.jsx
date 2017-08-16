fdca6fb7163fbaa1cd382ca9eef3637fe3e864d3
Plsr <christianpoplawski@gmail.com>
2017-08-16 10:47:42 +0200

    Basic structure for Layout & Grid root component

--- src/components/layout-grid/LayoutGrid.jsx
+++ src/components/layout-grid/LayoutGrid.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Redirect } from 'react-router'
+
+import SCOPES from '../../helpers/constants/scopes.js'
 
 export default class LayoutGrid extends React.Component {
+
+
   render () {
-    return (
-      <div>
-        Layout & Grid
-      </div>
-    )
+
+    // If no scope is set, return to home page
+    if ( this.props.scope == undefined ) {
+      return <Redirect to="/" />
+    }
+
+    if ( this.props.scope[0] === SCOPES.NATIVE_APP ) {
+      return (
+        <div>
+          Layout & Grid for Native
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Layout & Grid
+        </div>
+      )
+    }
+
   }
 }
