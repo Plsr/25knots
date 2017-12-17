730ddd08ab7009c0d383386f2df7a4ecf5595238
Plsr <christianpoplawski@gmail.com>
2017-12-17 16:51:23 +0100

    Remove redux logger

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore } from 'redux'
 import ApplicationState from './reducers/ApplicationState.js'
-import logger from 'redux-logger'
 
 import './styles/global.css'
 import './styles/normalize.css'
@@ -16,10 +15,7 @@ import SummaryContainer from './containers/SummaryContainer.js'
 
 let store = createStore(
   ApplicationState,
-  compose(
-    applyMiddleware(logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 ReactDOM.render(
